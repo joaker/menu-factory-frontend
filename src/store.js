36e72bc618859ef0895bbc8e6250571e709de7e1
joaker <jooakley@gmail.com>
2019-07-21 20:07:57 -0500

    add redux devtools

--- src/store.js
+++ src/store.js
@@ -1,6 +1,9 @@
 import { createStore } from 'redux';
 import reducer from 'reducer';
 
-export const store = createStore(reducer);
+const devtoolsEnhancer =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+export const store = createStore(reducer, undefined, devtoolsEnhancer);
 
 export default store;
